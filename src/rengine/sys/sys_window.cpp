5385dc90a91c2cd1479c1ecfa6f93bee5ed1d4a4
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-05-04 14:55:37 +0200

    Started working on creating a SDL Window context and then later once the entire Window system adn input and audio is setup I will pass that window context to the openGL and start drawing and working with the rendering part of the rEngine

--- src/rengine/sys/sys_window.cpp
+++ src/rengine/sys/sys_window.cpp
@@ -4,7 +4,7 @@
    Author: ksiric <email@example.com>
    Created: 2026-05-04 02:29:40
    Last Modified by: ksiric
-   Last Modified: 2026-05-04 03:03:49
+   Last Modified: 2026-05-04 14:54:28
    ---------------------------------------------------------------------
    Description:
        
@@ -14,7 +14,6 @@
    Version: 0.1.0
  ======================================================================
                                                                        */
-
 #include "rengine/sys/sys_window.h"
 #include "rengine/rcommon/com_print.h"
 
@@ -66,13 +65,45 @@ sys_error_code_t Sys_CreateWindow( const sys_window_desc_t &window_description, 
     return sys_error_code_t::OK;   
 }
 
-
-
-
-
+void Sys_DestroyWindow( sys_window_t &window ) {
+    SDL_Window *sdl_window = { nullptr };
+    if ( window.native_window == nullptr ) {
+        return ;
+    }  
     
+    sdl_window = static_cast<SDL_Window *>( window.native_window );
+    SDL_DestroyWindow( sdl_window );
+    SDL_QuitSubSystem( SDL_INIT_VIDEO ); 
+    window = {};
+   
+    return ; 
+}
+
+void Sys_PollWindowEvents( sys_window_t &window ) {
+    SDL_Event event{};
     
+    if ( window.native_window == nullptr ) {
+        return ;
+    }
     
-    
+    while ( SDL_PollEvent( &event ) ) {
+        switch( event.type ) {
+            case SDL_EVENT_QUIT:
+                window.should_close = true;
+                break;
+            case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+                window.should_close = true;
+                break;
+            case SDL_EVENT_WINDOW_RESIZED:
+                if ( event.window.data1 > 0 && event.window.data2 > 0 ) {
+                    window.width = static_cast<rcommon::u32>( event.window.data1 );
+                    window.height = static_cast<rcommon::u32>( event.window.data2 );
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
     
 }       // namespace reap::rengine::sys
